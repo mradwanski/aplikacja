f844e2eb945e24d69572ba4f227b0b57e54db1d5
mradwanski <mradwanski@plawniowice.pl>
2013-07-10 10:59:01 +0200

    Konfiguracja hibernate do połączenia z bazą PostgreSQL i pierwsza przykładowa klasa encyjna.

---
+++ miro/src/main/java/pl/miro/entity/User.java
@@ -0,0 +1,127 @@
+/***********************************************************
+* Elektroniczna Platforma Gromadzenia, Analizy 
+* i Udostępniania zasobów cyfrowych o Zdarzeniach Medycznych
+* 
+* Autorskie Prawa Majątkowe -
+* Centrum Systemów Informacyjnych Ochrony Zdrowia (CSIOZ).
+************************************************************
+* Wykonawca: Konsorcjum Kamsoft/Asseco Poland 
+* Osoba odpowiedzialna: Mirosław Radwański
+* Data utworzenia: 10-07-2013
+************************************************************/
+
+
+package pl.miro.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table (name = "blog_user")
+public class User {
+
+    
+    @Id
+    @GeneratedValue(generator = "account_id", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "account_id", sequenceName = "account_id_seq")
+    Integer id;
+    
+    @Column (name = "username")
+    private String userName;
+
+    @Column (name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column (name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column (name = "password", nullable = false)
+    private String password;
+
+    @Column (name = "created_on", nullable = false)
+    private Date createdOn;
+
+    @Transient
+    private Date lastLoginOn;
+
+    public User() {
+    }
+
+    public User(String userName, String firstName, String lastName, String password) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getLastLoginOn() {
+        return lastLoginOn;
+    }
+
+    public void setLastLoginOn(Date lastLoginOn) {
+        this.lastLoginOn = lastLoginOn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return userName.equals(user.userName);
+    }
+
+    @Override
+    public int hashCode() {
+        return 13 * userName.hashCode();
+    }
+}
